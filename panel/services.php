730903b2e4d8be4a653c0a18e97a44dfa2427f81
lazarevDev <lazarev.w3b@yandex.ru>
2023-05-14 08:45:10 +0300

    редактирование и удаление для services.php

--- panel/services.php
+++ panel/services.php
@@ -1,24 +1,64 @@
 <?php 
 require_once('../require/db.php');
 
+$arrayEdit = ['title' => null, 'category' => null, 'description' => null, 'price' => null, 'reception_time' => null];
 
+if(isset($_GET['edit'])){
+    $editId = $_GET['edit'];
+    
+    $queryEditService = mysqli_query($db, "SELECT * FROM `services` WHERE `id` = '$editId'");
+    $resultEditService = mysqli_fetch_array($queryEditService);
 
-if(isset($_POST['submit'])){
-    $title = $_POST['title'];
-    $category = $_POST['category'];
-    $price = $_POST['price'];
-    $description = $_POST['description'];
-    $receptionTime = $_POST['receptionTime'];
+    $arrayEdit = ['title' => $resultEditService['title'], 
+                'category' => $resultEditService['category'], 
+                'description' => $resultEditService['description'], 
+                'price' => $resultEditService['price'], 
+                'reception_time' => $resultEditService['reception_time']];
 
-    $queryAddService = "INSERT INTO `services` (`title`, `category`, `description`, `price`, `reception_time`) 
-    VALUES ('$title', '$category', '$description', '$price', '$receptionTime')";
-    $resultAddService = mysqli_query($db, $queryAddService) or die(mysqli_error($db));
+    if(isset($_POST['submit'])){
+        $title = $_POST['title'];
+        $category = $_POST['category'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $receptionTime = $_POST['receptionTime'];
+            
+        $querySuppliers = mysqli_query($db, "UPDATE `services` SET `title` = '$title', `category` = '$category', `price` = '$price', `description` = '$description', `reception_time` = '$receptionTime' WHERE `id` = '$editId'");
+        header('Location: services.php');
+        exit;
+    }
 
+}elseif(isset($_GET['delete'])){
+    $deleteId = $_GET['delete'];
 
+    $queryDelete = mysqli_query($db, "DELETE FROM `services` WHERE `id` = '$deleteId'");
+    header('Location: services.php');
+    exit;
+
+}else{
+    if(isset($_POST['submit'])){
+        $title = $_POST['title'];
+        $category = $_POST['category'];
+        $price = $_POST['price'];
+        $description = $_POST['description'];
+        $receptionTime = $_POST['receptionTime'];
+    
+        $queryAddService = "INSERT INTO `services` (`title`, `category`, `description`, `price`, `reception_time`) 
+        VALUES ('$title', '$category', '$description', '$price', '$receptionTime')";
+        $resultAddService = mysqli_query($db, $queryAddService) or die(mysqli_error($db));
+    }
 }
 
-
-
+function edit($form, $editInput){
+    if(isset($_GET['edit'])){
+        if(!empty($editInput)){
+            if($form == 'input'){
+                echo "value='".$editInput."'";
+            }elseif($form == 'textarea'){
+                echo $editInput;
+            }
+        }
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -43,20 +83,24 @@ if(isset($_POST['submit'])){
 
             <div class="content">
                 <form action="" class="formCategory" method="post">
-                    <input class="formCategoryInput" type="text" name="category" list="category" placeholder="Введите категорию">
+                    <input class="formCategoryInput" type="text" name="category" list="category" placeholder="Введите категорию" <?php edit('input', $arrayEdit['category']); ?>>
                     
                     <datalist id="category">
-                        <option value="Название1"> 
-                        <option value="Название2">
+                        <?php $queryServiceCategory = mysqli_query($db, "SELECT DISTINCT category FROM `services`");
+                        while($rowCategory = mysqli_fetch_array($queryServiceCategory)){
+                        $category = $rowCategory['category']; ?>
+                            <option value="<?php echo $category; ?>"> 
+                        <?php } ?>
                     </datalist>
                     
-                    <input class="formCategoryInput" name="title" type="text" placeholder="Введите название услуги">
-                    <input class="formCategoryInput" name="price" type="text" placeholder="Введите стоимость">
-                    <input class="formCategoryInput" name="receptionTime" type="text" placeholder="Введите время приема в минутах">
-                    <textarea name="description" id="" class="formCategoryInput formCategoryTextarea" placeholder="Введите описание для усгуи"></textarea>
+                    <input class="formCategoryInput" name="title" type="text" placeholder="Введите название услуги" <?php edit('input', $arrayEdit['title']); ?>>
+                    <input class="formCategoryInput" name="price" type="text" placeholder="Введите стоимость" <?php edit('input', $arrayEdit['price']); ?>>
+                    <input class="formCategoryInput" name="receptionTime" type="text" placeholder="Введите время приема в минутах" <?php edit('input', $arrayEdit['reception_time']); ?>>
+                    <textarea name="description" id="" class="formCategoryInput formCategoryTextarea" placeholder="Введите описание для услуг" ><?php edit('textarea', $arrayEdit['description']); ?></textarea>
 
                     <div class="formCategorySubmitContent">
                         <input class="formCategorySubmit" name="submit" type="submit" value="Опубликовать">
+                        <?php if(isset($_GET['edit'])){ ?><a class="formCategoryCancel" href="services.php">Отменить</a> <?php } ?>
                     </div>
                 </form>
             </div>
@@ -81,8 +125,8 @@ if(isset($_POST['submit'])){
                                     <p><?php echo $rowService['title']; ?> - <?php echo $rowService['price']; ?> руб.</p>
 
                                     <div class="servicesBlockActive">
-                                        <a class="edit" href="">Редактировать</a>
-                                        <a class="delete" href="">Удалить</a>
+                                        <a class="edit" href="services.php?edit=<?php echo $rowService['id']; ?>">Редактировать</a>
+                                        <a class="delete" href="services.php?delete=<?php echo $rowService['id']; ?>">Удалить</a>
                                     </div>
                                 </div>
                             <?php } ?>
